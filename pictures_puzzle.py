04f9a7e603bc4ba8872791696f6fff134095002b
Shubhanshu Nishad <69467788+Shubhanshu-Nishad@users.noreply.github.com>
2020-10-11 21:02:38 +0530

    Add files via upload

---
+++ pictures_puzzle.py
@@ -0,0 +1,19 @@
+from random import *
+from turtle import * 
+from freegames import floor, vector
+
+
+# # setup(420, 420, 370, 0)
+# circle(20)
+# hideturtle()
+# # tracer(False)
+# # load()
+# # draw()
+# # onscreenclick(tap)
+# done()
+
+for y in range(-200, 200, 100):
+    for x in range(-200, 200, 100):
+        print(x)
+
+    print('value of y is ',y)
