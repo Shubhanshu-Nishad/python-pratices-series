3cff62ea77d371366daeddacd0f73e0c4e3568d2
Shubhanshu Nishad <69467788+Shubhanshu-Nishad@users.noreply.github.com>
2020-10-21 10:37:55 +0530

    Add files via upload

---
+++ video-20/python20.py
@@ -0,0 +1,24 @@
+# Dictionary
+
+dic = {
+    'Name':'metaeducators',
+    'topic':'Dictionary',
+    'number':20
+} 
+# print(type(dic))
+# print(dic['Name'])
+# x = dic["Name"]
+# print(x)
+# print(dic.get('number'))
+dic['Name'] = 'shubhanshu'
+print(dic)
+# del dic['topic']
+# dic.pop('number')
+# dic.popitem()
+# dic.clear()
+# dict2 = dic.copy()
+# dict2 = dic
+# dic.pop('number')
+# print(dict2)
+# print(dic)
+
